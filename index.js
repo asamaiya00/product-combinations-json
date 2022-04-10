1341951b1839b3fca9dacae3f037f7de8995cc3d
Animesh Samaiya <asamaiya00@gmail.com>
2022-04-11 01:35:02 +0530

    feat: logic and html

--- index.js
+++ index.js
@@ -1,6 +1,12 @@
 class Product {
-  getPossibleCombinations(jsonObject) {
-    // console.log(jsonObject);
+  getPossibleCombinations() {
+    const jsonString = document.getElementById('json').value;
+
+    if (!jsonString.trim()) {
+      alert('Please enter JSON');
+      return;
+    }
+    const jsonObject = JSON.parse(jsonString);
     const obj = {};
     jsonObject.attributes.forEach((attr) => {
       obj[attr.name] = attr.values
@@ -8,128 +14,52 @@ class Product {
         .map((value) => value.name);
     });
 
-    // const pair = [];
-    // const ans = new Set();
+    // let result = Object.values(obj).reduce((mergedArray, currArray) => {
+    //   console.log(mergedArray, currArray);
+    //   return mergedArray.reduce((collectionArray, arrayTillNow) => {
+    //     console.log(collectionArray);
+    //     console.log(arrayTillNow);
+    //     return collectionArray.concat(
+    //       currArray.map((currElement) => [].concat(arrayTillNow, currElement))
+    //     );
+    //   }, []);
+    // });
 
-    // for (let key of Object.keys(obj)) {
-    //   console.log(key);
-    //   for (let property in obj[key]) {
-    //     pair.push({ [key]: obj[key][property] });
-    //     console.log(obj[key][property]);
-    //   }
-    // }
-    // console.log(pair);
+    const valuesArray = Object.values(obj);
 
-    Object.values(obj).forEach((valuesArray) => {
-      valuesArray.forEach((value) => {
-        console.log(value);
+    const productArray = valuesArray.reduce(
+      (arrayOfArray, currArray) => {
+        // console.log(arrayOfArray, currArray);
+        return arrayOfArray
+          .map((arrayTillNow) =>
+            currArray.map((newElement) => arrayTillNow.concat(newElement))
+          )
+          .flat();
+      },
+      [[]]
+    );
+
+    // console.log(productArray);
+
+    document.getElementById(
+      'output'
+    ).innerHTML += `<p> Total ${productArray.length} ${jsonObject.name}</p>`;
+    productArray.forEach((item) => {
+      const arr = [];
+      item.forEach((value, index) => {
+        arr.push(`${[Object.keys(obj)[index]]}: ${value}`);
       });
+      console.log(arr.join(' | '));
+      document.getElementById('output').innerHTML += `<p> ${arr.join(
+        ' | '
+      )} </p>`;
     });
-    return obj;
+    document.getElementById('output').innerHTML += '<br>';
   }
 }
 
-const input1 = `{
-    "name": "Lightning Cable",
-    "attributes": [
-        {
-            "name": "color",
-            "values": [
-                {
-                  "name": "red",
-                  "active": true
-                },
-                {
-                  "name": "blue",
-                  "active": true
-                },
-                {
-                  "name": "green",
-                  "active": true
-                }
-            ]
-        },
-        {
-            "name": "length",
-            "values": [
-                {
-                  "name": "1m",
-                  "active": true
-                },
-                {
-                  "name": "3m",
-                  "active": true
-                },
-                {
-                  "name": "6m",
-                  "active": true
-                }
-            ]
-        },
-        {
-            "name": "finish",
-            "values": [
-              {
-                "name": "nylon",
-                "active": true
-              },
-              {
-                "name": "rubber",
-                "active": false
-              }
-            ]
-        }
-    ]
-}`;
-
-const input2 = `{
-    "name": "Tshirt",
-    "attributes": [
-        {
-            "name": "color",
-            "values": [
-                {
-                  "name": "red",
-                  "active": true
-                },
-                {
-                  "name": "blue",
-                  "active": true
-                },
-                {
-                  "name": "green",
-                  "active": true
-                }
-            ]
-        },
-        {
-            "name": "size",
-            "values": [
-                {
-                  "name": "S",
-                  "active": true
-                },
-                {
-                  "name": "M",
-                  "active": false
-                },
-                {
-                  "name": "L",
-                  "active": true
-                }
-            ]
-        }
-    ]
-}`;
-
-const json = JSON.parse(input2);
 const product = new Product();
-const obj = product.getPossibleCombinations(json);
-console.log(obj);
-console.log(Object.values(obj));
-const obj2 = product.getPossibleCombinations(JSON.parse(input1));
 
-document.body.innerHTML =
-  JSON.stringify(json) + '<br><br>' + JSON.stringify(obj) + '<br><br>';
-document.body.innerHTML +=
-  '<br>' + JSON.stringify(json) + '<br><br>' + JSON.stringify(obj2);
+document
+  .getElementById('btn')
+  .addEventListener('click', () => product.getPossibleCombinations());
